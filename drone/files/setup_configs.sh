07df5f426acd42143b49107d857b8fc4b7b66d34
nacyot <propellerheaven@gmail.com>
2015-06-24 16:30:16 +0900

    Add GITLAB_CLIENT and GITLAB_SECRET envs

--- drone/files/setup_configs.sh
+++ drone/files/setup_configs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sed -i -e "s/::GITLAB_URL::/${GITLAB_URL}/g" /root/files/drone.toml
+sed -i -e "s/::GITLAB_CLIENT::/${GITLAB_CLIENT}/g" /root/files/drone.toml
+sed -i -e "s/::GITLAB_SECRET::/${GITLAB_SECRET}/g" /root/files/drone.toml
 sed -i -e "s/::WORKERS_URL::/${WORKERS_URL}/g" /root/files/drone.toml
 sed -i -e "s/::GITHUB_CLIENT::/${GITHUB_CLIENT}/g" /root/files/drone.toml
 sed -i -e "s/::GITHUB_SECRET::/${GITHUB_SECRET}/g" /root/files/drone.toml
